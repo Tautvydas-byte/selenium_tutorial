fb762a3532e73cc82a57cc6f51b677a7d131aeb1
Tautvydas <tautvydas.kvietkauskas@gmail.com>
2022-06-29 14:59:46 +0300

    ActionChains and automating cookie clicker

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 PATH = "C:/Users/tautv/Downloads/Programos/chromedriver_win32/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-driver.get('https://techwithtim.net')
+driver.get('https://orteil.dashnet.org/cookieclicker/')
 
-link = driver.find_element(By.LINK_TEXT, "Python Programming")
-link.click()
+driver.implicitly_wait(5)
+language = driver.find_element(By.ID, "langSelect-EN")
+language.click()
 
-try:
-	element = WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
-	)
-	element.click()
-	button = WebDriverWait(driver, 10).until(
-		EC.presence_of_element_located((By.ID, "sow-button-19310003"))
-	)
-	button.click()
+cookie = driver.find_element(By.ID, "bigCookie")
+cookie_count = driver.find_element(By.ID, "cookies")
+items = [driver.find_element(By.ID, "productPrice" + str(i)) for i in range(1, -1, -1)]  # to check the expensive first
 
-	driver.back()#back to previuos page
-	driver.back()
-	driver.back()
-	driver.forward()
-	driver.forward()
-except:
-	driver.quit()
+for i in range(5000):
+	actions = ActionChains(driver)
+	actions.click(cookie)
+	actions.perform()
+
+	count = int(cookie_count.text.split(" ")[0])
+	for item in items:
+		value = int(item.text)
+		if value <= count:
+			upgrade_actions = ActionChains(driver)
+			upgrade_actions.move_to_element(item)
+			upgrade_actions.click()
+			upgrade_actions.perform()
